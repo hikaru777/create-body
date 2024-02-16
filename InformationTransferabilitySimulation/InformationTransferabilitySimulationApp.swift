03b6faa7dd98f1a607148331537acaf80412c349
HikaruHonda <hikaru.h.developer@gmail.com>
2024-02-16 18:33:58 +0900

    Initial Commit

---
+++ InformationTransferabilitySimulation/InformationTransferabilitySimulationApp.swift
@@ -0,0 +1,17 @@
+//
+//  InformationTransferabilitySimulationApp.swift
+//  InformationTransferabilitySimulation
+//
+//  Created by 本田輝 on 2024/02/16.
+//
+
+import SwiftUI
+
+@main
+struct InformationTransferabilitySimulationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
